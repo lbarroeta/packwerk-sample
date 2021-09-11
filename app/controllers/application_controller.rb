0b784dcf4edc45fb510d300dfacc8c1ad5b7a795
Leonardo Barroeta <lbarroeta@kodim.tech>
2021-09-11 13:42:59 -0500

    Styled login and recover password views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_path
+  end
+
 end
--- config/routes/shared.rb
+++ config/routes/shared.rb
@@ -3,5 +3,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    root 'devise/sessions#new'
+
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get '/dashboard', to: 'shared/dashboards#index'
 end
